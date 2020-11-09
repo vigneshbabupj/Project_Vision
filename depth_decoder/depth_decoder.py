4de6291a42d3de926273201fccea6210ba603c5e
Vignesh Babu P J <vigneshbabu.pj@gmail.com>
2020-11-09 09:22:15 +0530

    Update depth_decoder.py

--- depth_decoder/depth_decoder.py
+++ depth_decoder/depth_decoder.py
@@ -7,7 +7,7 @@ https://github.com/thomasjpfan/pytorch_refinenet/blob/master/pytorch_refinenet/r
 import torch
 import torch.nn as nn
 
-from .blocks import FeatureFusionBlock, Interpolate
+from .blocks import FeatureFusionBlock, Interpolate, _make_encoder_scratch
 
 
 class MidasNet_decoder(nn.Module):
