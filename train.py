5e561c0cef3a378873ad35f53e472095109bfb60
Vignesh Babu P J <vigneshbabu.pj@gmail.com>
2020-11-21 00:37:30 +0530

    Update train.py

--- train.py
+++ train.py
@@ -523,7 +523,8 @@ def train(plane_args,yolo_args,midas_args,add_plane_loss,add_yolo_loss,add_midas
             #     #continue            
 
             #plane_loss = sum(plane_losses)
-            plane_loss = sum(plane_losses) /len(plane_losses) #vignesh to handle gradient explosion
+            print('plane_losses :',plane_losses)
+            plane_loss = (sum(plane_losses) /len(plane_losses)) #vignesh to handle gradient explosion
             plane_losses = [l.data.item() for l in plane_losses] #train_planercnn.py 331
 
             #print('plane_loss : ',plane_loss)
