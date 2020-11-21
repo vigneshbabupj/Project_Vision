c08668d7f12150f7b3b22b92c5ab014a69837cef
Vignesh Babu P J <vigneshbabu.pj@gmail.com>
2020-11-21 18:23:35 +0530

    Update train.py

--- train.py
+++ train.py
@@ -556,14 +556,13 @@ def train(plane_args,yolo_args,midas_args,add_plane_loss,add_yolo_loss,add_midas
             print('dp_prediction',dp_prediction.shape)
             print('dp_img_size',dp_img_size[:2])
 
-            temp = torch.nn.functional.interpolate(dp_prediction.unsqueeze(1),size=dp_img_size[:2])
 
             dp_prediction = (
                             torch.nn.functional.interpolate(
-                                dp_prediction.unsqueeze(1),
+                                dp_prediction.squeeze(),
                                 size=dp_img_size[:2],
                                 mode="bicubic",
-                                align_corners=False
+                                align_corners=False,
                             )
                             #.unsqueeze(0)
                             #.cpu()
