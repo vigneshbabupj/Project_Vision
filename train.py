5d6e17fed83dc3fc5fd133f6c2876a36f1d3a1b6
Vignesh Babu P J <vigneshbabu.pj@gmail.com>
2020-11-21 18:14:47 +0530

    Update train.py

--- train.py
+++ train.py
@@ -561,7 +561,7 @@ def train(plane_args,yolo_args,midas_args,add_plane_loss,add_yolo_loss,add_midas
                                 dp_prediction.unsqueeze(1),
                                 size=dp_img_size[:2],
                                 mode="bicubic",
-                                align_corners=False,
+                                align_corners=False
                             )
                             #.unsqueeze(0)
                             #.cpu()
@@ -599,7 +599,7 @@ def train(plane_args,yolo_args,midas_args,add_plane_loss,add_yolo_loss,add_midas
                                 depth_target.unsqueeze(1),
                                 size=dp_img_size[:2],
                                 mode="bicubic",
-                                align_corners=False,
+                                align_corners=False
                             )
                             #.unsqueeze(0)
                             #.cpu()
