4560ed2d07cd0d485cc7ea3b4abaf7b06e4aa13d
Vignesh Babu P J <vigneshbabu.pj@gmail.com>
2020-11-23 18:34:39 +0530

    Update __init__.py

--- pytorch_msssim/__init__.py
+++ pytorch_msssim/__init__.py
@@ -100,6 +100,8 @@ def msssim(img1, img2, window_size=11, size_average=True, val_range=None, normal
     pow1 = mcs ** weights
     pow2 = ssims ** weights
 
+    print('ssims :',ssims,'mcs:',mcs)
+
     # From Matlab implementation https://ece.uwaterloo.ca/~z70wang/research/iwssim/
     output = torch.prod(pow1[:-1] * pow2[-1])
     return output
