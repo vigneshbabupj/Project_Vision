4677d0cb47f86bd15fb06b4b8a601f58232c186f
Vignesh Babu P J <vigneshbabu.pj@gmail.com>
2020-11-28 15:21:10 +0530

    Update train.py

--- train.py
+++ train.py
@@ -549,7 +549,7 @@ def train(plane_args,yolo_args,midas_args,add_plane_loss,add_yolo_loss,add_midas
 
         
 
-            #print('plane_losses :',plane_losses)
+            print('plane_losses :',plane_losses)
             plane_loss = sum(plane_losses)
             plane_losses = [l.data.item() for l in plane_losses] #train_planercnn.py 331
 
