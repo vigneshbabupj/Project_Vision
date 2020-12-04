5e95392210a7ef0bc2b6e977a9db5a76b311f7dc
Vignesh Babu P J <vigneshbabu.pj@gmail.com>
2020-12-04 20:41:15 +0530

    Update train.py

--- train.py
+++ train.py
@@ -543,6 +543,11 @@ def train(plane_args,yolo_args,midas_args,add_plane_loss,add_yolo_loss,add_midas
             plane_parameters_pred = detection_pair[0]['detection'][:, 6:9]
             plane_masks_pred = detection_pair[0]['masks'][:, 80:560]
 
+            print(plane_parameters.shape)
+            print(plane_masks.shape)
+            print(plane_parameters_pred.shape)
+            print(plane_masks_pred.shape)
+
             
             plane_params_loss = loss_fn(plane_parameters_pred,plane_parameters) + loss_fn(plane_masks_pred,plane_masks)
 
